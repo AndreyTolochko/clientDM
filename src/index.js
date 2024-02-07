6cfadc0cb7b2852c4368d27085483a2a702bdb01
AndreyTolochko <45091849+AndreyTolochko@users.noreply.github.com>
2024-02-07 10:55:46 +0000

    added package @fvilers/disable-react-devtools and index.js file updated with production mode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthProvider';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthProvider'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
