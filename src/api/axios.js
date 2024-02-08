f925f63d51b64542ded419e040ed581abdde909c
AndreyTolochko <45091849+AndreyTolochko@users.noreply.github.com>
2024-02-08 18:30:39 +0000

    changed Base URL of the axios to deployed server url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:4000';
+
+//https://serverdm.onrender.com
+//'http://localhost:4000'
+const BASE_URL = 'https://serverdm.onrender.com';
 
 export default axios.create({
     baseURL:BASE_URL,
